d914c9a1a2698b063f1636d5659364060d001d80
Robert D'Agostino <rdag15@yahoo.com>
2021-07-20 17:36:52 -0400

    Add launch to URL's

--- bmi6300app/urls.py
+++ bmi6300app/urls.py
@@ -22,5 +22,6 @@ from medrec import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('patient/<int:pat_id>/', views.patient, name='patient')
+    path('patient/<int:pat_id>/', views.patient, name='patient'),
+    path('launch', views.launch, name="launch")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
