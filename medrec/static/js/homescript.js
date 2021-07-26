cb668ff2bdc1b203d08bfc75e7729848799aa724
Robert D'Agostino <rdag15@yahoo.com>
2021-07-26 17:31:34 -0400

    Add client state from updated FHIR JS docs

--- medrec/static/js/homescript.js
+++ medrec/static/js/homescript.js
@@ -2,19 +2,20 @@ function onError(data) {
     $('body').append(JSON.stringify(data));
 }
 
-function onReady(smart) {
+function onReady(client) {
     // Page receives access token from auth server
     // use the access token, and start fetching patient data
     // access token valid for 10 min
     console.log("This is onready");
     //access token
+    var smart = client.getState(); 
     var token = smart.tokenResponse.access_token;
     console.log(token);
     //patient ID
     var patient = smart.tokenResponse.patient;
     console.log(patient);
     //FHIR endpoint
-    var baseURL = smart.server.serviceUrl;
+    var baseURL = smart.serverUrl;
     console.log(baseURL);
     //start the app workflow
     // pass the access_token, patient ID, fhir endpoint
@@ -30,4 +31,7 @@ function onReady(smart) {
 //start here
 //when page is laoded the index receives code
 // this fhir function, exchanges code for access token
-FHIR.oauth2.ready(onReady, onError);
+FHIR.oauth2.ready()
+    .then(onReady(client))
+    .then(console.log)
+    .catch(console.error);
