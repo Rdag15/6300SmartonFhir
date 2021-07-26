4b00cb6d27b6377d843b1f687bc83ab08904ee98
Robert D'Agostino <rdag15@yahoo.com>
2021-07-25 23:33:43 -0400

    change case of smart in homescript

--- medrec/static/js/homescript.js
+++ medrec/static/js/homescript.js
@@ -2,19 +2,19 @@ function onError(data) {
     $('body').append(JSON.stringify(data));
 }
 
-function onReady(SMART) {
+function onReady(smart) {
     // Page receives access token from auth server
     // use the access token, and start fetching patient data
     // access token valid for 10 min
 
     //access token
-    var token = SMART.tokenResponse.access_token;
+    var token = smart.tokenResponse.access_token;
 
     //patient ID
-    var patient = SMART.tokenResponse.patient;
+    var patient = smart.tokenResponse.patient;
 
     //FHIR endpoint
-    var baseURL = SMART.server.serviceUrl;
+    var baseURL = smart.server.serviceUrl;
 
     //start the app workflow
     // pass the access_token, patient ID, fhir endpoint
